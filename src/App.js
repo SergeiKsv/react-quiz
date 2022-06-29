d740d1ee6c2191a034e7f1725a05d19d15f80873
Sergei Krasnoperov <92857381+SergeiKsv@users.noreply.github.com>
2022-06-29 12:20:34 +0600

    'Correct answers counter moved to App.js, added state for gameover screen'

--- src/App.js
+++ src/App.js
@@ -1,48 +1,59 @@
-import style from './App.module.css';
-import { HomeScreen } from './components/HomeScreen/HomeScreen';
-import { QuestionScreen } from './components/QuestionScreen/QuestionScreen';
-import { Footer } from './components/Footer/Footer';
-import { getQuestions } from './Api';
-import { useState } from 'react';
-
+import style from "./App.module.css";
+import { HomeScreen } from "./components/HomeScreen/HomeScreen";
+import { QuestionScreen } from "./components/QuestionScreen/QuestionScreen";
+import { Footer } from "./components/Footer/Footer";
+import { getQuestions } from "./Api";
+import { useState } from "react";
 
 const App = () => {
   const [questions, setQuestions] = useState([]);
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [answer, setAnswer] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
 
   const startGame = (amount, category, difficulty) => {
-    getQuestions(amount, category, difficulty).then(
-      items => setQuestions(items)
+    getQuestions(amount, category, difficulty).then((items) =>
+      setQuestions(items)
     );
-    
-  }
-  
+  };
+
+  const checkAnswer = (e) => {
+    if (e === true) setAnswer((prev) => prev + 1);
+  };
+
   const setQuestion = () => {
-    if (currentQuestion >= questions.length-1) {
+    if (currentQuestion >= questions.length - 1) {
       setCurrentQuestion(0);
       setQuestions(0);
+      setGameOver(true);
+    } else {
+      setCurrentQuestion(currentQuestion + 1);
     }
-    else {
-      setCurrentQuestion(currentQuestion+1);
-    }
+  };
+  let gameOverScreen;
+  if (gameOver) {
+    gameOverScreen = <div>{answer}</div>;
   }
-  
   return (
     <div className={style.AppWrapper}>
-      {
-        questions.length > 0 ?
-        <QuestionScreen nextQuestion={setQuestion}
+      {questions.length > 0 ? (
+        <QuestionScreen
+          nextQuestion={setQuestion}
           question={questions[currentQuestion]}
           currentQuestion={currentQuestion}
-          questionsCount={questions.length} /> :
-  
+          questionsCount={questions.length}
+          setAnswer={checkAnswer}
+          correctAnswer={answer}
+        />
+      ) : (
         <div className={style.wrapper}>
           <HomeScreen startGame={startGame} />
+          {gameOverScreen}
         </div>
-      }
-     <Footer/>
+      )}
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/QuestionScreen/QuestionScreen.jsx
+++ src/components/QuestionScreen/QuestionScreen.jsx
@@ -1,54 +1,84 @@
-import { useState, useEffect } from 'react';
-import styles from './Questions.module.css'
+import { useState, useEffect } from "react";
+import styles from "./Questions.module.css";
 
 export const QuestionScreen = (props) => {
-    const [correctAnswer, setAnswer] = useState(0);
-    const [classNames, setClassName] = useState({ correctAnswer: '', incorrectAnswer: '' });
-    const [display,setDisplay]=useState(true);
+  const [disabled, setDisabled] = useState(true);
+  const [classNames, setClassName] = useState({
+    correctAnswer: "",
+    incorrectAnswer: ""
+  });
 
-    useEffect(() => {
-        return () => {
-            setClassName('');
-            setDisplay(true);
-        };
-    }, [props])
+  useEffect(() => {
+    return () => {
+      setClassName("");
+      setDisabled(true);
+    };
+  }, [props.currentQuestion]);
 
-    const checkCorrectAnswer = (e) => {
-        setClassName({ ...classNames, correctAnswer: styles.correctAnswer, incorrectAnswer: styles.incorrectAnswer });
-        setDisplay(false);
-        if (e === true) {
-            setAnswer(correctAnswer + 1);
-        }
+  const checkCorrectAnswer = (e) => {
+    setClassName({
+      ...classNames,
+      correctAnswer: styles.correctAnswer,
+      incorrectAnswer: styles.incorrectAnswer
+    });
+    setDisabled(false);
+    if (e === true) {
+      props.setAnswer(e);
     }
+  };
 
-    return (
-        <div className={styles.QuestionScreen}>
-            <div className={styles.leftInfoBlock}>
-                <div className={styles.header}>
-                    <span>{props.question.category}</span>
-                </div>
-                <p className={styles.question}>{props.question.question}</p>
-                <div className={styles.questionsCountInfo}>
-                    <span>Question {props.currentQuestion+1} of {props.questionsCount} </span>
-                    <span>Correct answer: {correctAnswer} of {props.questionsCount}</span>
-                </div>
-            </div>
-            <div className={styles.rightInfoBlock}>
-                <div className={styles.header}>
-                    <span>Choose the correct answer</span>
-                </div>
-                <div className={styles.answersWrapper}>
-                    {props.question.answers.map(items => 
-                    <button className={styles.pageButtons + ' ' + styles.answers + ' '
-                        + `${items.isCorrect ? classNames.correctAnswer : classNames.incorrectAnswer}`}
-                            onClick={(e) => checkCorrectAnswer(items.isCorrect)}>{items.val}</button>)}
-                </div>
-                <div className={styles.questionControlGroup}>
-                    <button className={styles.pageButtons + ' ' + styles.questionControl}
-                        onClick={() => { props.nextQuestion() }} disabled={display}>Next Question</button>
-                </div>
-            </div>
+  return (
+    <div className={styles.QuestionScreen}>
+      <div className={styles.leftInfoBlock}>
+        <div className={styles.header}>
+          <span>{props.question.category}</span>
         </div>
-    );
-
-}
+        <p className={styles.question}>{props.question.question}</p>
+        <div className={styles.questionsCountInfo}>
+          <span>
+            Question {props.currentQuestion + 1} of {props.questionsCount}{" "}
+          </span>
+          <span>
+            Correct answer: {props.correctAnswer} of {props.questionsCount}
+          </span>
+        </div>
+      </div>
+      <div className={styles.rightInfoBlock}>
+        <div className={styles.header}>
+          <span>Choose the correct answer</span>
+        </div>
+        <div className={styles.answersWrapper}>
+          {props.question.answers.map((items) => (
+            <button
+              className={
+                styles.pageButtons +
+                " " +
+                styles.answers +
+                " " +
+                `${
+                  items.isCorrect
+                    ? classNames.correctAnswer
+                    : classNames.incorrectAnswer
+                }`
+              }
+              onClick={(e) => checkCorrectAnswer(items.isCorrect)}
+            >
+              {items.val}
+            </button>
+          ))}
+        </div>
+        <div className={styles.questionControlGroup}>
+          <button
+            className={styles.pageButtons + " " + styles.questionControl}
+            onClick={() => {
+              props.nextQuestion();
+            }}
+            disabled={disabled}
+          >
+            Next Question
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
